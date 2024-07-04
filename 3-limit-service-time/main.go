d9e35ddcead26d89dbd28ea104bf8ccb0c7adca8
jaennil <jaennil@yandex.ru>
2024-07-04 18:29:43 +0300

    feat: limit service time

--- 3-limit-service-time/main.go
+++ 3-limit-service-time/main.go
@@ -1,14 +1,11 @@
-//////////////////////////////////////////////////////////////////////
-//
-// Your video processing service has a freemium model. Everyone has 10
-// sec of free processing time on your service. After that, the
-// service will kill your process, unless you are a paid premium user.
-//
-// Beginner Level: 10s max per request
-// Advanced Level: 10s max per user (accumulated)
-//
-
 package main
+
+import (
+	"sync"
+	"time"
+)
+
+const SECONDS_PER_USER = 10
 
 // User defines the UserModel. Use this to check whether a User is a
 // Premium user or not
@@ -16,13 +13,35 @@ type User struct {
 	ID        int
 	IsPremium bool
 	TimeUsed  int64 // in seconds
+	lock      sync.Mutex
 }
 
 // HandleRequest runs the processes requested by users. Returns false
 // if process had to be killed
 func HandleRequest(process func(), u *User) bool {
-	process()
-	return true
+
+	processDone := make(chan struct{})
+
+	go func() {
+		process()
+		processDone <- struct{}{}
+
+	}()
+
+	for {
+		select {
+		case <-time.After(time.Second):
+			u.lock.Lock()
+			if !u.IsPremium && u.TimeUsed >= 10 {
+				u.lock.Unlock()
+				return false
+			}
+			u.TimeUsed += 1
+			u.lock.Unlock()
+		case <-processDone:
+			return true
+		}
+	}
 }
 
 func main() {
